0d3e408a527f68934f6759044e7dba78d644cc58
Gary Benson <gary@gbenson.net>
2023-06-10 23:35:10 +0100

    Add a basic reporting tool

---
+++ src/nx/admin/cli.py
@@ -0,0 +1,63 @@
+import time
+
+from .redis import Redis
+
+
+class Reporter(Redis):
+    def report(self):
+        self._report_machines()
+        self._report_heartbeats()
+
+    def _report_heartbeats(self):
+        heartbeats = self.hgetall("heartbeats")
+        if not heartbeats:
+            return
+        print("Heartbeats:")
+        now = time.mktime(time.localtime())
+        for worker, seen in sorted(heartbeats.items()):
+            delta = int(now - float(seen))
+            seen = self._strftime(seen)
+            print(f"  {worker:7}: {delta:2} seconds ago : {seen}")
+        print()
+
+    def _report_machines(self):
+        keys = list(self.scan_iter("mac_*"))
+        if not keys:
+            return
+        pipeline = self.pipeline()
+        for key in keys:
+            pipeline.hgetall(key)
+        machines = zip(keys, pipeline.execute())
+        for _, key, machine in sorted(
+                (float(machine["last_seen"]), key, machine)
+                for key, machine in machines
+        ):
+            print(f"{key}:")
+            for field, value in sorted(machine.items()):
+                if (field.startswith("last_DHCP")
+                        and not field.endswith("_seen")):
+                    if len(field.split("_")) == 2:
+                        value = repr(value)
+                    if len(value) > 80:
+                        value = value[:77] + "..."
+                print(f"  {field:26}: {value}")
+            print()
+
+    @classmethod
+    def _strftime(cls, timestamp, fmt="%Y-%m-%d %H:%M:%S"):
+        secs, frac = timestamp, None
+        if isinstance(secs, str):
+            secs, frac = timestamp.split(".", 1)
+        secs = float(secs)
+        result = time.strftime(fmt, time.localtime(secs))
+        if frac is not None:
+            result = f"{result}.{frac:7}"
+        return result
+
+    @classmethod
+    def main(cls):
+        with cls(decode_responses=True) as r:
+            r.report()
+
+
+main = Reporter.main
