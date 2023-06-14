5e652b683e29fb8dbf140f1ebe3d5cf8ed79f42b
Gary Benson <gary@gbenson.net>
2023-06-14 22:00:35 +0100

    New worker (Zack)

---
+++ src/nx/workers/zack.py
@@ -0,0 +1,20 @@
+from ..common import PacketSnifferWorker
+
+
+class DHCPMonitorWorker(PacketSnifferWorker):
+    WORKER_NAME = "Zack"
+    WANTED_PACKETS = "port 53"
+
+    def process_packet(
+            self,
+            packet,
+            pipeline,
+            macaddr,
+            mac_key,
+            common_fields,
+            **kwargs
+    ):
+        pass
+
+
+main = DHCPMonitorWorker.main
