7dcb3b206b58257b27ca617d538322c850031383
Gary Benson <gary@gbenson.net>
2023-06-19 20:41:43 +0100

    Copy all sniffed HTTP packets from slice into local Redis

---
+++ httpcopy.py
@@ -0,0 +1,23 @@
+from redis import Redis as LocalRedis
+from nx.admin.redis import Redis as SliceRedis
+
+def main():
+    src = SliceRedis()
+    dst = LocalRedis()
+
+    total = 0
+    pipeline = dst.pipeline(transaction=False)
+    for key in src.scan_iter("pkt_*"):
+        saved_packet = src.hgetall(key)
+        if b"last_seen_by_ethan" not in saved_packet:
+            continue
+        pipeline.hset(key, mapping=saved_packet)
+        if len(pipeline) >= 100:
+            total += len(pipeline)
+            pipeline.execute()
+            print(f"Done {total}")
+    print(f"Flushing final {len(pipeline)}...")
+    pipeline.execute()
+
+if __name__ == "__main__":
+    main()
