c473a34c9b47811325f0f2468de9db37fc3dbec5
Gary Benson <gary@gbenson.net>
2023-06-22 23:55:19 +0100

    Steve and his C2

---
+++ src/nx/admin/ineedyou.py
@@ -0,0 +1,87 @@
+import base64
+import hashlib
+import hmac
+import os
+import readline
+
+from .redis import Redis
+
+
+class RedisC2Client:
+    def __init__(self, worker_name="Steve"):
+        WORKER_NAME = worker_name.upper()
+        self.worker_name = worker_name.lower()
+
+        secret = os.environ[f"{WORKER_NAME}_SECRET"]
+        if not isinstance(secret, bytes):
+            secret = secret.encode("utf-8")
+        self.hmac = hmac.new(secret, digestmod=hashlib.blake2s)
+
+        self.pub_chan = f"send-to:{self.worker_name}"
+        self.sub_chan = f"recv-from:{self.worker_name}"
+
+        self.histfile = os.path.expanduser("~/.ineedyou_history")
+        self.prompt = f"{self.worker_name}> "
+
+    def run(self):
+        self.db = Redis()
+
+        self.ps = self.db.pubsub()
+        self.ps.subscribe(self.sub_chan)
+
+        try:
+            readline.read_history_file(self.histfile)
+        except FileNotFoundError:
+            pass
+        readline.set_history_length(2000)
+
+        try:
+            self.main_loop()
+        finally:
+            readline.write_history_file(self.histfile)
+
+    def main_loop(self):
+        self._send(f"hello {self.worker_name}")
+
+        while True:
+            text = self._read_response()
+            if text is not None:
+                end = "" if text.endswith("\n") else "\n"
+                print(text, end=end)
+
+            try:
+                command = input(self.prompt)
+            except EOFError:  # Ctrl-D
+                print("exit")
+                break
+            if command == "exit":
+                break
+
+            self._send(self._sign(command.encode("utf-8")))
+
+    def _sign(self, request):
+        hash = self.hmac.copy()
+        hash.update(self.challenge)
+        hash.update(request)
+        # Line below mimics secrets.token_urlsafe()
+        sig = base64.urlsafe_b64encode(hash.digest()).rstrip(b"=")
+        return b":".join((sig, request))
+
+    def _read_response(self):
+        for msg in self.ps.listen():
+            if msg is None:
+                continue
+            if msg["type"] != "message":
+                continue
+            response = msg["data"].split(b":", 1)
+            self.challenge = response[0]
+            if len(response) != 2:
+                return None
+            return response[1].decode("utf-8")
+
+    def _send(self, data):
+        self.db.publish(self.pub_chan, data)
+
+
+def main():
+    RedisC2Client().run()
---
+++ src/nx/workers/steve.py
@@ -0,0 +1,93 @@
+import base64
+import hashlib
+import hmac
+import logging
+import os
+import secrets
+import traceback
+
+from ..common import RedisClientWorker
+
+log = logging.getLogger(__name__)
+
+
+class RedisC2Worker(RedisClientWorker):
+    WORKER_NAME = "Steve"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        secret = self._load_secret(kwargs)
+        if not isinstance(secret, bytes):
+            secret = secret.encode("utf-8")
+        self.hmac = hmac.new(secret, digestmod=hashlib.blake2s)
+
+        self._reset_challenge()
+
+        self.pub_chan = f"recv-from:{self.name}"
+        self.sub_chan = f"send-to:{self.name}"
+
+        self.greet_me = f"hello {self.name}".encode("utf-8")
+
+    def _load_secret(self, kwargs):
+        """Obtain the shared secret.  Modifies kwargs."""
+        secret = kwargs.pop("secret", None)
+        if secret is not None:
+            return secret
+        secvar = kwargs.pop("secret_env", None)
+        if secvar is None:
+            secvar = f"{self.name.upper()}_SECRET"
+        return os.environ[secvar]
+
+    def _reset_challenge(self):
+        self.challenge = secrets.token_urlsafe()
+
+    def run(self):
+        super().run()
+        with self.db.pubsub() as p:
+            p.subscribe(self.sub_chan)
+            log.info(f"listening on: {self.sub_chan!r}")
+            log.info(f"publishing to: {self.pub_chan!r}")
+            self.main_loop(p)
+
+    def main_loop(self, p):
+        for msg in p.listen():
+            self._checkpoint_worker()
+            if msg is None:
+                continue
+            if msg["type"] != "message":
+                continue
+
+            request = msg["data"]
+            log.info(f"cmd: {request!r}")
+
+            if request == self.greet_me:
+                response = self.challenge
+            else:
+                response = self._handle_request(request)
+                response = f"{self.challenge}:{response}"
+            response = response.encode("utf-8")
+
+            log.info(f"out: {response!r}")
+            self.db.publish(self.pub_chan, response)
+
+    def _handle_request(self, request):
+        try:
+            return self.handle_request(request)
+        except Exception:
+            return "".join(traceback.format_exc())
+
+    def handle_request(self, request):
+        recv_sig, request = request.split(b":", 1)
+        hash = self.hmac.copy()
+        hash.update(self.challenge.encode("ascii"))
+        self._reset_challenge()
+        hash.update(request)
+        # Line below mimics secrets.token_urlsafe()
+        good_sig = base64.urlsafe_b64encode(hash.digest()).rstrip(b"=")
+        if not hmac.compare_digest(recv_sig, good_sig):
+            raise ValueError("bad signature")
+        return str(eval(request.decode("utf-8")))
+
+
+main = RedisC2Worker.main
