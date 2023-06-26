ff65a491b4bae34d9c38516678f459700296e7cb
Gary Benson <gary@gbenson.net>
2023-06-26 01:35:46 +0100

    Remove setup for now-removed special sniffing Python binary

--- deploy.sh
+++ deploy.sh
@@ -11,17 +11,8 @@ cat <<EOF
 export PS4="-|\$USER@\$HOSTNAME| "
 set -exuo pipefail
 [ -f /opt/nx/workers/bin/activate ] || python3 -m venv /opt/nx/workers
-ln -sf /opt/nx/bin/python3nx /opt/nx/workers/bin
 source /opt/nx/workers/bin/activate
 pip install --upgrade /var/lib/nx/wheels/$wheel
-set +x
-for file in /opt/nx/workers/bin/*; do
-  name=\$(basename \$file)
-  [ -d /home/\$name ] || continue
-  id \$name | grep -q '(nx-sniffers)' || continue
-  echo \$PS4 sed -i 's/python3\$/python3nx/' \$file
-  sed -i 's/python3\$/python3nx/' \$file
-done
 EOF
 )
 
