d24def8c391faf8459717f53bec3599184963473
Gary Benson <gary@gbenson.net>
2023-07-02 21:37:26 +0100

    Hello Susan

---
+++ src/nx/workers/susan.py
@@ -0,0 +1,96 @@
+from scapy.all import DNS, IP, TCP, dnsclasses, dnsqtypes
+
+from ..common.constants import MINUTES, WEEKS
+from ..roles.packet_sniffer import PacketSnifferWorker, UnhandledPacket
+
+
+class DNSMonitorWorker(PacketSnifferWorker):
+    WORKER_NAME = "Susan"
+    WANTED_PACKETS = "port 53"
+
+    def process_packet(self, packet, **kwargs):
+        dns = packet.getlayer(DNS)
+        if dns is None:
+            self.process_non_dns_packet(packet)
+            return
+
+        if dns.opcode != 0:  # QUERY
+            raise UnhandledPacket("dns_opcode")
+
+        is_query = dns.qr == 0  # query/response
+        is_response = dns.qr == 1
+        if is_query is not (not is_response):
+            raise UnhandledPacket("dns_qr")
+
+        if is_query:
+            self.process_query(packet, dns)
+            return
+
+        # Don't retain packets we don't (yet) process.
+        self.expire_packet_after(30 * MINUTES)
+
+    def process_query(self, packet, dns):
+        if dns.rcode != 0:  # NOERROR
+            raise UnhandledPacket("dns_query_rcode")
+        if dns.tc:  # truncated
+            raise UnhandledPacket("dns_query_tc")
+
+        # For queries the IPv4 src is on our network.
+        ipv4 = packet.getlayer(IP)
+        if ipv4 is not None:
+            self.record_ipv4_sighting(ipv4.src)
+
+        if dns.qdcount != 1:
+            self.record_issue("dns_query_qdcount")
+        for index in range(dns.qdcount):
+            self.record_dns_lookup(packet, dns.qd[index])
+
+        # Most of these will be UDP and will hash-collide.  TCP ones
+        # won't collide, but there shouldn't be anything like as many
+        # of those.
+        self.expire_packet_after(4 * WEEKS)
+
+    def record_dns_lookup(self, packet, question):
+        pipeline = self.pipeline
+        question = ":".join((
+            question.qname.decode(),
+            dnsclasses[question.qclass],
+            dnsqtypes[question.qtype],
+        ))
+
+        key = f"dnsq:{question}"
+        fields = {
+            "last_seen": packet.time,
+            "last_seen_in": self.packet_hash,
+            "last_seen_from": self.src_mac,
+            f"last_seen_from_{self.src_mac}": packet.time,
+        }
+        pipeline.hset(key, mapping=fields)
+        for field in ("first_seen", f"first_seen_from_{self.src_mac}"):
+            pipeline.hsetnx(key, field, packet.time)
+
+        key = f"dnsq_pkts:{question}"
+        pipeline.hset(key, self.packet_hash, packet.time)
+
+        key = self.mac_key
+        fields = {
+            "last_dns_query": question,
+            "last_dns_query_seen": packet.time,
+        }
+        pipeline.hset(key, mapping=fields)
+
+    TCP_CONNECTION_ADMIN_FLAGS = {"S", "SA", "A", "FA"}
+
+    def process_non_dns_packet(self, packet):
+        tcp = packet.getlayer(TCP)
+        if tcp is None:
+            raise UnhandledPacket("port_53")
+
+        if tcp.flags not in self.TCP_CONNECTION_ADMIN_FLAGS:
+            raise UnhandledPacket("port_53_tcp")
+
+        # Don't retain (presumably) connection admin packets.
+        self.expire_packet_after(30 * MINUTES)
+
+
+main = DNSMonitorWorker.main
---
+++ tests/test_susan.py
@@ -0,0 +1,119 @@
+from scapy.all import Ether, IP, UDP, DNS, DNSQR
+
+from nx.workers.susan import DNSMonitorWorker
+
+
+def make_test_packet(**kwargs):
+    _kwargs, kwargs = kwargs, dict(
+        hwsrc="00:0D:F7:12:CA:FE",
+        src="1.2.3.4",
+        dst="5.6.7.8",
+        layer3=UDP,
+        sport=12345,
+        dport=53,
+        qr=0,  # question
+        qname=b"nx.example.com.",
+        qclass=1,  # IN
+        qtype=1,  # A
+    )
+    kwargs.update(**_kwargs)
+
+    packet = Ether(
+        src=kwargs["hwsrc"],
+        dst="c8:e1:30:ba:be:23",
+    ) / IP(
+        src=kwargs["src"],
+        dst=kwargs["dst"],
+    ) / kwargs["layer3"](
+        sport=kwargs["sport"],
+        dport=kwargs["dport"],
+    ) / DNS(
+        id=12345,
+        qr=kwargs["qr"],
+        qd=DNSQR(
+            qname=kwargs["qname"],
+            qclass=kwargs["qclass"],
+            qtype=kwargs["qtype"],
+        ),
+    )
+
+    packet = packet.__class__(_pkt=bytes(packet))
+    packet.time = 1686086875.268219
+    packet.sniffed_on = "wlx0023cafebabe"
+    print(f"{packet!r} => {packet.original!r}")
+
+    return packet
+
+
+def test_basic_ipv4_udp_query(mockdb):
+    """Basic IPv4 UDP DNS queries are logged as expected."""
+    worker = DNSMonitorWorker(mockdb)
+    packet = make_test_packet()
+    worker._process_packet(packet)
+
+    expect_packet_hash = ("518f310855e1cee99362844f2d351ede"
+                          "32fc4512e98ee6279578ebead0b42e:3")
+    expect_packet_key = f"pkt_{expect_packet_hash}"
+
+    assert worker.db.log == [
+        ["hset", expect_packet_key, [
+            ("last_seen", 1686086875.268219),
+            ("last_seen_by", "susan"),
+            ("last_seen_by_susan", 1686086875.268219),
+            ("last_seen_from", "00:0d:f7:12:ca:fe"),
+            ("last_sniffed_on", "wlx0023cafebabe"),
+            ("raw_bytes", b">>raw bytes<<"),
+        ]],
+        ["hsetnx", expect_packet_key, [
+            ("first_seen", 1686086875.268219),
+        ]],
+        ["hincrby", (expect_packet_key, "num_sightings", 1)],
+        ["hset", "interfaces", [
+            ("wlx0023cafebabe", 1686086875.268219),
+        ]],
+        ["sadd", "macs", ("00:0d:f7:12:ca:fe",)],
+        ["hset", "mac_00:0d:f7:12:ca:fe", [
+            ("last_seen", 1686086875.268219),
+            ("last_seen_by", "susan"),
+            ("last_seen_by_susan", 1686086875.268219),
+        ]],
+        ["hsetnx", "mac_00:0d:f7:12:ca:fe", [
+            ("first_seen", 1686086875.268219),
+        ]],
+        ["hset", "macpkts_00:0d:f7:12:ca:fe", [
+            (expect_packet_hash, 1686086875.268219),
+        ]],
+        ["hset", "mac_00:0d:f7:12:ca:fe", [
+            ("ipv4", "1.2.3.4"),
+        ]],
+        ["hset", "ipv4_1.2.3.4", [
+            ("last_seen", 1686086875.268219),
+            ("last_seen_by", "susan"),
+            ("last_seen_by_susan", 1686086875.268219),
+            ("mac", "00:0d:f7:12:ca:fe"),
+        ]],
+        ["sadd", "ipv4s", ("1.2.3.4",)],
+        ["hset", "dnsq:nx.example.com.:IN:A", [
+            ("last_seen", 1686086875.268219),
+            ("last_seen_from", "00:0d:f7:12:ca:fe"),
+            ("last_seen_from_00:0d:f7:12:ca:fe", 1686086875.268219),
+            ("last_seen_in", expect_packet_hash),
+        ]],
+        ["hsetnx", "dnsq:nx.example.com.:IN:A", [
+            ("first_seen", 1686086875.268219),
+        ]],
+        ["hsetnx", "dnsq:nx.example.com.:IN:A", [
+            ("first_seen_from_00:0d:f7:12:ca:fe", 1686086875.268219),
+        ]],
+        ["hset", "dnsq_pkts:nx.example.com.:IN:A", [
+            (expect_packet_hash, 1686086875.268219),
+        ]],
+        ["hset", "mac_00:0d:f7:12:ca:fe", [
+            ("last_dns_query", "nx.example.com.:IN:A"),
+            ("last_dns_query_seen", 1686086875.268219),
+        ]],
+        ["expire", expect_packet_key, 2419200],
+        ["hset", "heartbeats", [
+            ("susan", 1686086875.268219),
+        ]],
+        "execute"]
